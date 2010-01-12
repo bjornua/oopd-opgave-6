93962ad2c98dd0e5344b7bc6367b7708c1731f56
Klaus Møllnitz <klaus.mollnitz@gmail.com>
2010-01-12 11:54:44 +0100

    Tilføjede class MainWindow samt constructor

---
+++ src/galapagos6/view/MainWindow.java
@@ -0,0 +1,20 @@
+package galapagos6.view;
+
+import java.util.Observable;
+import java.util.Observer;
+import galapagos6.model.*;
+import galapagos6.control.*;
+
+public class MainWindow implements Observer {
+
+	public MainWindow(Biotope biotope, BiotopeController biotopeController) {
+		
+	}
+	
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
